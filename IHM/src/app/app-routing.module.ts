3b4a074597a6c6ba4326cf6ca564736d7b3e4b7d
Mathieu Veys <oominimathoo@gmail.com>
2019-08-02 17:03:24 +0200

    add nav bar / structure

--- IHM/src/app/app-routing.module.ts
+++ IHM/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent } from './components/pages/index/index.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: IndexComponent},
+  { path: 'about', component: ProfilComponent },
+  { path: 'profil', component: AboutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- IHM/src/app/app.module.ts
+++ IHM/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/layout/nav-bar/nav-bar.component';
+import { IndexComponent } from './components/pages/index/index.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
