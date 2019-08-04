c0a357587019e9fcebec58c4c8941130a921abef
PulcoOx - Home <oominimathoo@gmail.com>
2019-08-04 18:58:06 +0200

    models actor + listing card

--- IHM/src/app/components/pages/actor/actor-list/actor-list.component.ts
+++ IHM/src/app/components/pages/actor/actor-list/actor-list.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Actor } from '../../../../models/actor.model';
 
 @Component({
   selector: 'app-actor-list',
   templateUrl: './actor-list.component.html',
   styleUrls: ['./actor-list.component.scss']
 })
+
 export class ActorListComponent implements OnInit {
 
-  constructor() { }
+  actor: Actor = {} as Actor;
+  constructor( private http: HttpClient ) { }
 
   ngOnInit() {
+    this.getActor();
   }
 
+  getActor(): void
+  {
+    this.http.get<Actor>('http://localhost:9090/actor')
+    .pipe(map(data => data))
+      .subscribe(
+        (actor: Actor)=>{
+        this.actor = actor;
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
 }
