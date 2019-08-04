7bbf4434180cac3095c49e81468c32936648263e
PulcoOx - Home <oominimathoo@gmail.com>
2019-08-04 16:46:46 +0200

    add composant actorList - actorDetail

--- IHM/src/app/app.module.ts
+++ IHM/src/app/app.module.ts
@@ -8,6 +8,10 @@ import { NavBarComponent } from './components/layout/nav-bar/nav-bar.component';
 import { IndexComponent } from './components/pages/index/index.component';
 import { MapComponent } from './components/pages/map/map.component';
 import {HttpClientModule} from '@angular/common/http';
+import { ActorDetailComponent } from './components/pages/actor/actor-detail/actor-detail.component';
+import { ActorListComponent } from './components/pages/actor/actor-list/actor-list.component';
+import { RechercheComponent } from './components/pages/recherche/recherche.component';
+
 
 
 
@@ -16,7 +20,10 @@ import {HttpClientModule} from '@angular/common/http';
     AppComponent,
     NavBarComponent,
     IndexComponent,
-    MapComponent
+    MapComponent,
+    ActorDetailComponent,
+    ActorListComponent,
+    RechercheComponent,
   ],
   imports: [
     BrowserModule,
