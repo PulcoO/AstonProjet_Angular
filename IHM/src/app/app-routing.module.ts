2aed88ddfa2194b417969773e5f2c1ba3279bb15
PulcoOx <oominimathoo@gmail.com>
2019-08-27 17:36:07 +0200

    master
    card actor

--- IHM/src/app/app-routing.module.ts
+++ IHM/src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
   { path: '', component: IndexComponent},
   { path: 'map', component: MapComponent},
   { path: 'actorList', component: ActorListComponent},
-  { path: 'actor/Detail', component: ActorDetailComponent},
+  { path: 'actor/:actorId', component: ActorDetailComponent},
 ];
 
 @NgModule({
--- IHM/src/app/components/pages/actor/actor-detail/actor-detail.component.ts
+++ IHM/src/app/components/pages/actor/actor-detail/actor-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Actor } from '../../../../models/actor.model';
+import { Category } from '../../../../models/category.model';
+import { HttpClient } from '@angular/common/http';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-actor-detail',
@@ -6,10 +10,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./actor-detail.component.scss']
 })
 export class ActorDetailComponent implements OnInit {
-
-  constructor() { }
+  actor: Actor = {} as Actor;
+  categories: Category = {} as Category;
+  constructor(private http: HttpClient,private route: ActivatedRoute) { 
+    this.route.params.subscribe( params => console.log(params) )
+    this.route.params.subscribe( params => {
+      let actorId = params.actorId;
+      let url = 'http://localhost:9090/actor/' + actorId;
+      this.getActor(url);
+      
+    })
+  }
 
   ngOnInit() {
   }
 
+  getActor(url): void
+  {
+    this.http.get<Actor>(url)
+    // .pipe(map(data => data))
+      .subscribe(
+        (data: Actor)=>{
+          this.actor = data;
+           //console.log(this.actor)
+        },
+      err => {
+        console.log(err);
+      }
+    );
+  }
 }
+
